75c6dc1c99fc575aed40fdeffeeb400eeac3f593
Jiovane Ferreira <jiovane.ferreira@gmail.com>
2023-11-08 00:02:09 -0300

    exemplo Hero Card, TailwindCSS

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,22 @@ function Pagina() {
 
     <div className="container mx-auto">
 
-     <div className="w-12 h-12 border-8 border-blue-200 border-r-blue-600 rounded-full animate-spin"></div>
+     {/*} <div className="w-12 h-12 border-8 border-blue-200 border-r-blue-600 rounded-full animate-spin">
+      </div> */}
 
+      <div className="rounded-xl overflow-hidden p-6 bg-slate-800 md:flex md:p-0">
+        <div className="w-24 h-24 rounded-full bg-cover bg-center mx-auto md:w-48 md:h-auto md:rounded-none bg-[url(https://s2.glbimg.com/4HtuLBE5Bj_itQJj2AlL5A9ss50=/e.glbimg.com/og/ed/f/original/2021/05/05/gettyimages-476706986.jpg)]">
+
+        </div>
+        <div className="grow pt-6 text-center md:p-8 md:text-left">
+          <p className="text-lg text-white">"Eu não sou o Nicolas Cage"</p>
+          <p className="mt-3 font-bold text-sky-400">Marilyn Manson</p>
+          <p className="text-slate-500">Cantor, EUA</p>
+        </div>
+
+      </div>
     </div>
-
-    
+  
   
   )
 }
